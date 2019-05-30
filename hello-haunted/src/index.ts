7c849a502a988889c4ba9f56ef1653e41ed2b5e6
otofu-square <otofu.square@gmail.com>
2019-05-30 18:33:31 +0900

    :wrench: improve haunted-counter

--- hello-haunted/src/index.ts
+++ hello-haunted/src/index.ts
@@ -1,21 +1,49 @@
 import { html, render } from "lit-html";
-import { component, useState } from "haunted";
+import { component, useState, useEffect } from "haunted";
+import jss from "jss";
+import preset from "jss-preset-default";
+
+jss.setup(preset());
+
+const { classes } = jss
+  .createStyleSheet({
+    wrapper: {
+      padding: 40,
+      background: "#f7df1e",
+      textAlign: "center"
+    },
+    count: {
+      color: "#24292e"
+    }
+  })
+  .attach();
 
 function Counter() {
   const [count, setCount] = useState(0);
+
+  useEffect(
+    () => {
+      console.log("count changed: " + count);
+    },
+    [count]
+  );
+
   return html`
-    <div id="count">${count}</div>
-    <button type="button" @click="${() => setCount(count + 1)}">
-      Increment
-    </button>
+    <div class="${classes.wrapper}">
+      <h1 class="${classes.count}">${count}</h1>
+      <div>
+        <button type="button" @click="${() => setCount(count + 1)}">+</button>
+        <button type="button" @click="${() => setCount(count - 1)}">-</button>
+      </div>
+    </div>
   `;
 }
 
-customElements.define("remember-april", component(Counter));
+customElements.define("haunted-counter", component(Counter));
 
 render(
   html`
-    <remember-april></remember-april>
+    <haunted-counter></haunted-counter>
   `,
   document.getElementById("app")
 );
